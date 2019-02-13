33ca3e6704ba4a3d82a1170f70ad5e56b9de9a7e
netravler <pheintz@hotmail.com>
2019-02-13 15:00:23 -0600

    Add files via upload

---
+++ KillTree/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace KillTree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKillTree = new System.Windows.Forms.Button();
+            this.tbKillProcess = new System.Windows.Forms.TextBox();
+            this.rtbKillTree = new System.Windows.Forms.RichTextBox();
+            this.tbFindProcess = new System.Windows.Forms.TextBox();
+            this.btnFindProcess = new System.Windows.Forms.Button();
+            this.btnKillForest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnKillTree
+            // 
+            this.btnKillTree.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnKillTree.Location = new System.Drawing.Point(12, 38);
+            this.btnKillTree.Name = "btnKillTree";
+            this.btnKillTree.Size = new System.Drawing.Size(221, 23);
+            this.btnKillTree.TabIndex = 0;
+            this.btnKillTree.Text = "KillTree";
+            this.btnKillTree.UseVisualStyleBackColor = false;
+            this.btnKillTree.Click += new System.EventHandler(this.btnKillTree_Click);
+            // 
+            // tbKillProcess
+            // 
+            this.tbKillProcess.BackColor = System.Drawing.Color.LightGray;
+            this.tbKillProcess.Location = new System.Drawing.Point(12, 12);
+            this.tbKillProcess.Name = "tbKillProcess";
+            this.tbKillProcess.Size = new System.Drawing.Size(453, 20);
+            this.tbKillProcess.TabIndex = 1;
+            // 
+            // rtbKillTree
+            // 
+            this.rtbKillTree.BackColor = System.Drawing.Color.LightGray;
+            this.rtbKillTree.Location = new System.Drawing.Point(12, 126);
+            this.rtbKillTree.Name = "rtbKillTree";
+            this.rtbKillTree.Size = new System.Drawing.Size(453, 505);
+            this.rtbKillTree.TabIndex = 2;
+            this.rtbKillTree.Text = "";
+            this.rtbKillTree.Click += new System.EventHandler(this.rtbKillTree_Click_1);
+            this.rtbKillTree.TextChanged += new System.EventHandler(this.rtbKillTree_TextChanged);
+            // 
+            // tbFindProcess
+            // 
+            this.tbFindProcess.BackColor = System.Drawing.Color.LightGray;
+            this.tbFindProcess.Location = new System.Drawing.Point(12, 67);
+            this.tbFindProcess.Name = "tbFindProcess";
+            this.tbFindProcess.Size = new System.Drawing.Size(453, 20);
+            this.tbFindProcess.TabIndex = 3;
+            // 
+            // btnFindProcess
+            // 
+            this.btnFindProcess.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnFindProcess.Location = new System.Drawing.Point(12, 93);
+            this.btnFindProcess.Name = "btnFindProcess";
+            this.btnFindProcess.Size = new System.Drawing.Size(453, 23);
+            this.btnFindProcess.TabIndex = 4;
+            this.btnFindProcess.Text = "Find Process";
+            this.btnFindProcess.UseVisualStyleBackColor = false;
+            this.btnFindProcess.Click += new System.EventHandler(this.btnFindProcess_Click);
+            // 
+            // btnKillForest
+            // 
+            this.btnKillForest.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnKillForest.Location = new System.Drawing.Point(244, 38);
+            this.btnKillForest.Name = "btnKillForest";
+            this.btnKillForest.Size = new System.Drawing.Size(221, 23);
+            this.btnKillForest.TabIndex = 5;
+            this.btnKillForest.Text = "KillForest";
+            this.btnKillForest.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnKillForest.UseVisualStyleBackColor = false;
+            this.btnKillForest.Click += new System.EventHandler(this.btnKillForest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 643);
+            this.Controls.Add(this.btnKillForest);
+            this.Controls.Add(this.btnFindProcess);
+            this.Controls.Add(this.tbFindProcess);
+            this.Controls.Add(this.rtbKillTree);
+            this.Controls.Add(this.tbKillProcess);
+            this.Controls.Add(this.btnKillTree);
+            this.Name = "Form1";
+            this.Text = "Kill Process Tree - Forest";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKillTree;
+        private System.Windows.Forms.TextBox tbKillProcess;
+        private System.Windows.Forms.RichTextBox rtbKillTree;
+        private System.Windows.Forms.TextBox tbFindProcess;
+        private System.Windows.Forms.Button btnFindProcess;
+        private System.Windows.Forms.Button btnKillForest;
+    }
+}
+
---
+++ KillTree/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Management;
+using System.Windows.Forms;
+
+namespace KillTree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resetbtnKillForestList();
+            
+        }
+
+        /// Kill a process, and all of its children, grandchildren, etc.
+        /// <param name="pid">Process ID.</param>
+
+        private static void KillProcessAndChildren(int pid)
+        {
+            // Cannot close 'system idle process'.
+            if (pid == 0)
+            {
+                return;
+            }
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher
+                    ("Select * From Win32_Process Where ParentProcessID=" + pid);
+            ManagementObjectCollection moc = searcher.Get();
+            foreach (ManagementObject mo in moc)
+            {
+                KillProcessAndChildren(Convert.ToInt32(mo["ProcessID"]));
+            }
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                proc.Kill();
+            }
+            catch (ArgumentException)
+            {
+                // Process already exited.
+            }
+        }
+
+        private void rtbKillTree_Click_1(object sender, EventArgs e)
+        {
+            rtbKillTree.Clear();
+
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process theprocess in processlist)
+            {
+                /*Console.WriteLine("Process: {0} ID: {1}", theprocess.ProcessName, theprocess.Id); */
+                rtbKillTree.Text += "Process > " + theprocess.ProcessName + " ID: " + theprocess.Id + Environment.NewLine;
+            }
+        }
+
+        private void btnFindProcess_Click(object sender, EventArgs e)
+        {
+            StringBuilder holdProcessFound = new StringBuilder();
+
+            foreach (string line in rtbKillTree.Lines)
+                if (line.Contains(tbFindProcess.Text.Trim()))
+                {
+                    holdProcessFound.Append(line.ToString() + Environment.NewLine);
+                }
+                    
+            // clear process list and fill with found
+
+            rtbKillTree.Clear();
+
+            rtbKillTree.Text += holdProcessFound;
+
+            // ENABLE forest kill...
+
+            btnKillForest.Enabled = true;
+        }
+
+        private void btnKillTree_Click(object sender, EventArgs e)
+        {
+            int removePid = Convert.ToInt32(tbKillProcess.Text);
+            KillProcessAndChildren(removePid);
+        }
+
+        private void btnKillForest_Click(object sender, EventArgs e)
+        {
+            // I was going to do a combined function but went with a standalone for killing all found...
+            /*
+             * ok:)  This function will take whatever has been found in rtbKillTree and take the pid on the end and kill it!
+             * My advise is to make sure that you did a find first otherwise bad things will happen:)
+             * 
+             * You've been warned!!!
+             * 
+             */
+
+            int fstart = 0;
+            int fend = 0;
+
+            // get rtbkilltree by line
+
+            string[] RichTextBoxLines = rtbKillTree.Lines;
+            string outCome = "";
+
+            foreach (string line in RichTextBoxLines)
+            {
+                // get everything after the ":" clean it up and then kill it!...
+
+                //outCome += line.Substring(line.IndexOf(':'), line.Length -1);
+                //count = count + 1;
+
+                // this is a test...
+
+                fstart = line.IndexOf(":");
+                fend = line.Length - fstart;
+
+                outCome = line.Substring(fstart + 1, fend - 1);
+
+                // killForest Member...
+
+                if (line.Length > 0)
+                {
+                    KillProcessAndChildren(Convert.ToInt32(outCome));
+                }
+
+            }
+
+            // refresh rtbKillTree list, general cleanup...
+
+            resetbtnKillForestList();
+     }
+
+        private void resetbtnKillForestList()
+        {
+            rtbKillTree.Clear();
+
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process theprocess in processlist)
+            {
+                /*Console.WriteLine("Process: {0} ID: {1}", theprocess.ProcessName, theprocess.Id); */
+                rtbKillTree.Text += "Process> " + theprocess.ProcessName + " ID: " + theprocess.Id + Environment.NewLine;
+            }
+
+            btnKillForest.Enabled = false;
+        }
+
+        private void rtbKillTree_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
